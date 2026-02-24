67a36ed7a108af97597f153126dc630f8292302b
Sharpienero <sharpienero@gmail.com>
2026-02-23 20:56:25 -0500

    Bump to 1.5.9

--- src/main/java/gg/embargo/ui/EmbargoPanel.java
+++ src/main/java/gg/embargo/ui/EmbargoPanel.java
@@ -304,7 +304,7 @@ public class EmbargoPanel extends PluginPanel {
         versionPanel.setLayout(new BoxLayout(versionPanel, BoxLayout.Y_AXIS));
 
         // Set up Embargo Clan Version at top of Version panel
-        JLabel version = new JLabel(htmlLabel("Embargo Clan Version: ", "1.5.8"));
+        JLabel version = new JLabel(htmlLabel("Embargo Clan Version: ", "1.5.9"));
         version.setFont(smallFont);
         version.setAlignmentX(Component.LEFT_ALIGNMENT);
 
